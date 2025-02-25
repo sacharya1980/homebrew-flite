70d8a536201a4c55bfc120a178fafaa78695d4b2
Sankalp Acharya <sacharya@rmn.com>
2025-02-24 23:25:40 -0600

    add Makefile patch for macOS

--- Formula/flite.rb
+++ Formula/flite.rb
@@ -21,3 +21,33 @@ class Flite < Formula
     system "#{bin}/flite", "-t", "Hello Homebrew!"
   end
 end
+
+__END__
+diff --git a/main/Makefile b/main/Makefile
+index 1da18a8..8166182 100644
+--- a/main/Makefile
++++ b/main/Makefile
+@@ -34,6 +34,13 @@
+ ##  Top level user programs                                              ##
+ ##                                                                       ##
+ ###########################################################################
++# MacOS does not accept -pd options to the cp command
++    UNAME_S := $(shell uname -s)
++ifeq ($(UNAME_S),Darwin)
++CP_FLAGS='-r'
++else
++CP_FLAGS='-pd'
++endif
+ TOP=..
+ DIRNAME=main
+ BUILD_DIRS = 
+@@ -152,8 +159,8 @@ install:
+ 	done
+ 	$(INSTALL) -m 755 $(BINDIR)/flite_time $(DESTDIR)$(INSTALLBINDIR)
+ #       The libraries: static and shared (if built)
+-	cp -pd $(flite_LIBS_deps) $(DESTDIR)$(INSTALLLIBDIR)
++	cp $(CP_FLAGS) $(flite_LIBS_deps) $(DESTDIR)$(INSTALLLIBDIR)
+ ifdef SHFLAGS
+-	cp -pd $(SHAREDLIBS) $(VERSIONSHAREDLIBS) $(DESTDIR)$(INSTALLLIBDIR)
++	cp $(CP_FLAGS) $(SHAREDLIBS) $(VERSIONSHAREDLIBS) $(DESTDIR)$(INSTALLLIBDIR)
+ endif
